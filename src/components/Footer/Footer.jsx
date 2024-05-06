158debd44e7c73fc791a30eefd67cc2b46a67bf6
Avinash Kumar <avinash.k95347@gmail.com>
2024-05-06 16:44:00 +0530

    Update footer content in Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,16 @@
 const Footer = () => {
   return (
     <footer className="w-full bg-lightBlack py-5 md:py-10 px-5">
-      <div className="container mx-auto text-white">
-        <span className="text-sm md:text-base tracking-wider">&copy; copyright SLIETshare { new Date().getFullYear()}</span>
+      <div className="container mx-auto">
+        <div className="w-full text-center mb-8">
+          <p className="text-sm md:text-base tracking-wider text-gray-300 inline">A platform of the Slietiens for the Slietiens by the Slietiens <img className="inline relative bottom-[1.2px]" src="assets/icons/red-heart-icon.svg" alt="" /></p>
+          
+        </div>
+        <div>
+        <p className="text-sm md:text-base tracking-wider text-gray-500">&copy; copyright SLIETshare {new Date().getFullYear()}</p>
+        <p className="text-xs mt-3 text-warn">Please report any error at <a href="mailto:info@slietshare.online"> info@slietshare.online</a></p>
+        </div>
+       
       </div>
     </footer>
   );
