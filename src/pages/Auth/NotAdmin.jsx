baea87073a18625d9a694b556000e584e0f62a1a
Avinash Kumar <avinash.k95347@gmail.com>
2024-05-06 15:17:04 +0530

    Update link in NotAdmin.jsx and NotAuthenticated.jsx to point to the correct page

--- src/pages/Auth/NotAdmin.jsx
+++ src/pages/Auth/NotAdmin.jsx
@@ -18,7 +18,7 @@ function NotAdmin() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path>
                 </svg>
             </div>
-            <p className="mt-4 text-gray-600">Let's get you back <Link to="" className="text-blue-500">login</Link>.</p>
+            <p className="mt-4 text-gray-600">Let's get you back <Link to="/" className="text-blue-500">home</Link>.</p>
         </div>
         )
 }
--- src/pages/Auth/NotAuthenticated.jsx
+++ src/pages/Auth/NotAuthenticated.jsx
@@ -19,7 +19,7 @@ function NotAuthenticated() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path>
                 </svg>
             </div>
-            <p className="mt-4 text-gray-600">Let's get you back <Link to="" className="text-blue-500">login</Link>.</p>
+            <p className="mt-4 text-gray-600">Let's get you back <Link to="/login" className="text-blue-500">login</Link>.</p>
         </div>
         )
 }
