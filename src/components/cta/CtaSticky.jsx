30ad32b268d625b84596512de75d3d2c068b72a5
Avinash Kumar <avinash.k95347@gmail.com>
2024-05-06 18:07:26 +0530

    Fix link in CtaSticky.jsx to navigate to the correct page

--- src/components/cta/CtaSticky.jsx
+++ src/components/cta/CtaSticky.jsx
@@ -1,23 +1,27 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 const CtaSticky = () => {
     // const [expanded, setExpanded] = useState(false);
-
+    const navigate = useNavigate();
     return (
-        <Link to="qs/upload" className="cta-sticky fixed right-5 bottom-5 md:bottom-14 z-5">
-            {/* <div className={'overflow-hidden trasition duration-300 flex flex-col justify-center w-full '+ (expanded?'h-20':'h-0') }>
-                <div>a</div>
-                <div>b</div>
-                <div>c</div>
-            </div> */}
-            <button className="h-12 w-12 md:h-16 md:w-16" style={{
-                backgroundImage: `url('/assets/icons/plus.svg')`,
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center',
-                backgroundSize: 'cover'
-            }}></button>
-        </Link>
+            <div className="cta-sticky fixed right-5 bottom-5 md:bottom-14 z-[20]">
+                {/* <div className={'overflow-hidden trasition duration-300 flex flex-col justify-center w-full '+ (expanded?'h-20':'h-0') }>
+                        <div>a</div>
+                        <div>b</div>
+                        <div>c</div>
+                    </div> */}
+                <button className="h-12 w-12 md:h-16 md:w-16" style={{
+                    backgroundImage: `url('/assets/icons/plus.svg')`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover'
+                }}
+                    onClick={() => {
+                        navigate('/qs/upload')
+                    }}
+                ></button>
+            </div>
     )
 }
 export default CtaSticky
