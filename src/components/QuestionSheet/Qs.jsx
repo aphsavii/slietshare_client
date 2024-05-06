2a8010209a01d6a92f12e779fdd80fab84badbaf
Avinash Kumar <avinash.k95347@gmail.com>
2024-05-06 23:31:56 +0530

    Update image rendering and date format in Qs.jsx

--- src/components/QuestionSheet/Qs.jsx
+++ src/components/QuestionSheet/Qs.jsx
@@ -7,8 +7,8 @@ const Qs = ({ uploader, subName, subCode, qsUrl, DOE, type, ctaText, ctaFunc }) 
   return (
     <div className=" flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border h-fit rounded-xl w-64 md:w-72">
       <a href={qsUrl} target="_blank" rel="noreferrer">
-        <div className="h-32 md:h-48 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
-          <img src={qsUrl} alt="qs" />
+        <div className="h-32 md:h-48 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40 ">
+          <img className="w-full h-full object-contain" src={qsUrl.includes('pdf')?'assets/icons/pdf.svg':qsUrl} alt="qs" />
         </div>
       </a>
       <div className="px-6 py-3">
@@ -19,7 +19,7 @@ const Qs = ({ uploader, subName, subCode, qsUrl, DOE, type, ctaText, ctaFunc }) 
         <div className="font-sans text-xs md:text-sm antialiased font-light leading-relaxed text-inherit flex justify-between ">
           <span> {subCode}</span> <span className="italic text-primary font-medium">By {uploader.split(" ")[0]}</span>
         </div>
-        <span className="text-xs md:text-sm text-gray-500">{type} ({DOE.slice(6)})</span>
+        <span className="text-xs md:text-sm text-gray-500">{type} ({DOE.slice(0,4)})</span>
       </div>
       <div className="p-6 pt-0">
     {ctaText &&  <BtnPrimary onClick={ctaFunc} text={ctaText}  />}
