e5ea7bcb60719abeab477445e0e11607f40608a3
Avinash Kumar <avinash.k95347@gmail.com>
2024-05-10 13:53:58 +0530

    Update Alert component styles in Alert.jsx

--- src/components/alerts/Alert.jsx
+++ src/components/alerts/Alert.jsx
@@ -15,11 +15,11 @@ const Alert = ({ message, type, }) => {
   if (type === "error") {
     return (
       <div
-        className={` min-w-[250px] fixed mx-auto z-[10] left-1/2 -translate-x-1/2 top-16 md:top-24 flex items-center justify-between px-5 py-3 md:px-5 md:py-5 mb-5 leading-normal text-red-600 bg-red-100 rounded-lg`}
+        className={` min-w-[280px] fixed mx-auto z-[10] left-1/2 -translate-x-1/2 top-16 md:top-24 flex items-center justify-between px-5 py-2.5 md:px-5 md:py-5 mb-5 leading-normal text-red-600 bg-red-100 rounded-lg`}
         role="alert"
       >
-        <p>{message}</p>
-        <span className="ml-5 cursor-pointer" onClick={removeAlert}>
+        <p className="text-base md:text-lg">{message}</p>
+        <span className=" ml-2 md:ml-5 cursor-pointer" onClick={removeAlert}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6 text-red-600"
@@ -40,11 +40,11 @@ const Alert = ({ message, type, }) => {
   } else if (type === "success") {
     return (
       <div
-        className="min-w-[250px] fixed mx-auto z-[10] left-1/2 -translate-x-1/2 top-16 md:top-24 flex items-center justify-between px-5 py-3 md:p-5 mb-5 leading-normal text-green-600 bg-green-100 rounded-lg"
+        className="min-w-[280px] fixed mx-auto z-[10] left-1/2 -translate-x-1/2 top-16 md:top-24 flex items-center justify-between px-5 py-2.5 md:p-5 mb-5 leading-normal text-green-600 bg-green-100 rounded-lg"
         role="alert"
       >
-        <p>{message}</p>
-        <span className="ml-5 cursor-pointer" onClick={removeAlert}>
+        <p className="text-base md:text-lg">{message}</p>
+        <span className=" ml-2 md:ml-5 cursor-pointer" onClick={removeAlert}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6 text-green-600"
