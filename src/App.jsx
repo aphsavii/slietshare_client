767ea1e58550c78426c1def8db86142148179da3
Avinash Kumar <avinash.k95347@gmail.com>
2024-06-05 01:04:23 +0530

    Update authentication logic in App.jsx, Layout.jsx, AdminRoutes.jsx, and AuthRoutes.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -52,35 +52,6 @@ const routes = createRoutesFromElements(
 
 // App component
 function App() {
-  const dispatch = useDispatch();
-  const accessToken = sessionStorage.getItem('accessToken');
-  const user = JSON.parse(localStorage.getItem('user'));
-  const refreshToken = localStorage.getItem('refreshToken');
-  const isRefreshTokenValid = refreshToken && jwtDecode(refreshToken).exp > Date.now() / 1000;
-  useEffect(() => {
-    if (accessToken && user && isRefreshTokenValid) {
-      dispatch(loginSuccess({ user, accessToken, refreshToken }));
-      return;
-    }
-    if (isRefreshTokenValid && !accessToken) {
-      dispatch(setAppLoading(true));
-      userAuthService.refreshTokens().then((res) => {
-        if (res?.user && res?.accessToken) {
-          dispatch(loginSuccess({ user: res.user, accessToken: res.accessToken, refreshToken: res.refreshToken }));
-        }
-        dispatch(setAppLoading(false));
-      }).catch((e) => {
-        console.error(e);
-        dispatch(setAppLoading(false));
-        dispatch(loginFailure('Session expired'));
-      })
-    }
-    else {
-      dispatch(loginFailure('Session expired'));
-    }
-  });
-
-
   const router = createBrowserRouter(routes);
   const appLoading = useSelector((state) => state.loading.appLoading);
 
--- src/components/routes/AuthRoutes/AuthRoutes.jsx
+++ src/components/routes/AuthRoutes/AuthRoutes.jsx
@@ -1,10 +1,43 @@
 import { Outlet, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch } from "react-redux";
+import { useEffect } from 'react';
+import { loginSuccess,loginFailure } from '../../../redux/slices/auth';
+import { jwtDecode } from 'jwt-decode';
+import userAuthService from '../../../api/services/userAuthService';
+import { setAppLoading } from '../../../redux/slices/appLoading/index.js';
+
 
 const AuthRoutes = () => {
-  const { isAuthenticated } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const accessToken = sessionStorage.getItem('accessToken');
+  const user = JSON.parse(localStorage.getItem('user'));
+  const refreshToken = localStorage.getItem('refreshToken');
+  const isRefreshTokenValid = refreshToken && jwtDecode(refreshToken).exp > Date.now() / 1000;
+  useEffect(() => {
+    if (accessToken && user && isRefreshTokenValid) {
+      dispatch(loginSuccess({ user, accessToken, refreshToken }));
+      return;
+    }
+    if (isRefreshTokenValid && !accessToken) {
+      dispatch(setAppLoading(true));
+      userAuthService.refreshTokens().then((res) => {
+        if (res?.user && res?.accessToken) {
+          dispatch(loginSuccess({ user: res.user, accessToken: res.accessToken, refreshToken: res.refreshToken }));
+        }
+        dispatch(setAppLoading(false));
+      }).catch((e) => {
+        console.error(e);
+        dispatch(setAppLoading(false));
+        dispatch(loginFailure('Session expired'));
+      })
+    }
+    else {
+      dispatch(loginFailure('Session expired'));
+    }
+  });
+
   return (
-    isAuthenticated ? <Outlet /> : <Navigate to='/noAuth' />
+    accessToken ? <Outlet /> : <Navigate to='/noAuth' />
   )
 }
 
