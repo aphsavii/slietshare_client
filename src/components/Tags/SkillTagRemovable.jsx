42f7192fa08a8453a570f8802b741cfc10f2126f
Avinash Kumar <avinash.k95347@gmail.com>
2024-06-24 19:19:03 +0530

    Add SkillTag and SkillTagRemovable components

---
+++ src/components/Tags/SkillTagRemovable.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function SkillTagRemovable({
+    text,
+    proficiency = 'Beginner',
+    className,
+    crossFunc
+}) {
+  const skillProficiency = {
+    Beginner: 'bg-gray-100 text-gray-500 border-gray-200',
+    Intermediate: 'bg-yellow-100 text-yellow-500 border-yellow-200',
+    Advanced: 'bg-green-100 text-green-600 border-green-200',
+    };
+
+  return (
+    <div title={text + ' '+ proficiency} className={`px-4 md:px-5 text-sm lg:text-base font-medium py-1 h-fit w-fit rounded-2xl lg:rounded-3xl border-[1px] ${skillProficiency[proficiency]}  ${className}`}>
+     <span> {text}</span> <span className='ml-3 text-gray-700' onClick={crossFunc}>X</span>
+    </div>
+  )
+}
+
+export default SkillTagRemovable;
