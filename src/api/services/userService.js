11ab632e32362d81105f556ef6bf062a038c0aa7
Avinash Kumar <avinash.k95347@gmail.com>
2024-06-24 19:18:14 +0530

    Fix axiosAuthInstance usage in getUserDetails function

--- src/api/services/userService.js
+++ src/api/services/userService.js
@@ -3,7 +3,7 @@ import { apiHandler } from "../utils/apiHandler";
 
 const userService = {
     getUserDetails: async (regno) => {
-        const {data, error} = apiHandler(()=>axiosAuthInstance.get('user/'+regno));
+        const {data, error} = await apiHandler(()=> axiosAuthInstance.get('user/'+regno));
         return data ?? error ;
     }
 }
