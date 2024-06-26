a4b314dfa7d1c095fba6c497183d48e538106460
Avinash Kumar <avinash.k95347@gmail.com>
2024-06-26 23:53:06 +0530

    Add toMonthYear function to helpers/index.js

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,14 @@
+
 const textCapitalize = (text) => {
     return text.charAt(0).toUpperCase() + text.slice(1);
 }
 
-export {textCapitalize}
+const toMonthYear=(date)=>{
+    if(date==="Present") return date;
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = d.toLocaleString('en-US', { month: 'long' });
+    return month+" "+year;
+}
+
+export {textCapitalize,toMonthYear }
