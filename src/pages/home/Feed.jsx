65650c7f2c44a7fec45c8ab6add1bf828630c956
Avinash Kumar <avinash.k95347@gmail.com>
2024-07-18 02:43:46 +0530

    fix

---
+++ src/pages/home/Feed.jsx
@@ -0,0 +1,79 @@
+import { isMobile } from "@/helpers";
+import React from "react";
+import Post from "@/components/Post/Post";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import FeedService from "@/api/services/FeedService";
+import PostSkeleton from "@/components/skeletons/PostSkeleton.jsx";
+import PostSidebar from "@/components/post-sidebar/PostSidebar";
+import { useDispatch } from "react-redux";
+import { toggleCreatePost } from "@/redux/slices/popups";
+function Feed() {
+  const {
+    data,
+    error,
+    status,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["posts"],
+    queryFn: ({ pageParam = 1 }) =>
+      FeedService.getFeedPosts({ pageParam, limit: 5 }),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length ? allPages.length + 1 : undefined;
+    },
+  });
+
+  const { ref, inView } = useInView();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (error) console.error(error);
+    if (status === "success" && inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, inView, error, status, hasNextPage]);
+
+
+  return (
+    <div className="container min-h-[600px] md:min-h-[800px] mx-auto px-5 py-10">
+      <div className="w-full flex gap-5 relative lg:justify-around lg:gap-10 lg:px-40">
+        <div id="post-container" className="w-full lg:w-4/6 border ">
+          {!data?.pages && <PostSkeleton />}
+          {data?.pages &&
+            data.pages.map((page, i) => (
+              <React.Fragment key={i}>
+                {page.map((post) => (
+                  <Post key={post._id} post={post} />
+                ))}
+              </React.Fragment>
+            ))}
+          <div className="" ref={ref}>
+            {isFetchingNextPage && <PostSkeleton />}
+            {data && !hasNextPage && (
+              <>
+              <p className="text-center text-gray-500">No more posts</p>
+              <p className="text-center text-gray-500">You are most welcome to <span onClick={()=>dispatch(toggleCreatePost())} className="text-primaryBlue underline cursor-pointer">
+              create one</span></p>
+              </>
+              
+            )}
+          </div>
+        </div>
+        {!isMobile() && (
+          <div
+            id="suggested-profiles"
+            className="w-1/3 border min-h-[140px] h-fit bg-white sticky right-0 top-0 rounded-lg hidden lg:block"
+          >
+            <PostSidebar />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
