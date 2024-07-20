63cc90e5909f780534ebfd55bbaf75433d613fae
Avinash Kumar <avinash.k95347@gmail.com>
2024-07-20 11:13:25 +0530

    Refactor import paths in jsconfig.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ import { defineConfig } from "vite"
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": new URL("./src", import.meta.url).pathname,
-    },
+    alias: [{ find: '@', replacement: "/src" }],
   },
 })
